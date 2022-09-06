00e2757f20c90c8d78e5b6c8f4bb82b17b594502
Leonardo Santos <leonardojosantos@gmail.com>
2022-09-06 17:03:07 -0300

    Criando uma aplicação console

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("teste");
+
+int numVezes = 5;
+
+for (int i = 0; i < numVezes; i++)
+{
+    Console.WriteLine($"Bem vindo ao curso de .NET {i+1}º");
+}
+
