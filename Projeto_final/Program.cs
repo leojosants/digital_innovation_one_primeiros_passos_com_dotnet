3892f8fa6fc139c6c0d6519bc9a2e99b8f22386d
Leonardo Santos <leonardojosantos@gmail.com>
2022-09-08 16:31:27 -0300

    Projeto Final - Parte 1

---
+++ Projeto_final/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+namespace Projeto_final
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("\n");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: Adicionar aluno
+                        break;
+
+                    case "2":
+                        //TODO: Listar alunos
+                        break;
+
+                    case "3":
+                        //TODO: Calcular média geral
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("1 - Inserir novo aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Calcular média geral");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();
+
+            }
+
+        }
+
+    }
+
+}
