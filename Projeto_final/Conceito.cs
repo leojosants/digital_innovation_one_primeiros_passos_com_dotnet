ad8d5cbcae26898807da562cb10ef86d3e25057a
Leonardo Santos <leonardojosantos@gmail.com>
2022-09-08 17:38:45 -0300

    Dinâmica de desenvolvimento

---
+++ Projeto_final/Conceito.cs
@@ -0,0 +1,16 @@
+
+namespace Projeto_final
+{
+
+    public enum Conceito
+    {
+
+        A,
+        B,
+        C,
+        D,
+        E
+
+    }
+
+}
--- Projeto_final/Program.cs
+++ Projeto_final/Program.cs
@@ -12,7 +12,7 @@ namespace Projeto_final
             Console.Write("\n");
 
             Aluno[] alunos = new Aluno[5];
-            
+
             var indiceAluno = 0;
 
             string opcaoUsuario = ObterOpcaoUsuario();
@@ -52,7 +52,44 @@ namespace Projeto_final
                         break;
 
                     case "3":
-                        //TODO: Calcular média geral
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i = 0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+
+                        if (mediaGeral < 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral < 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral < 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral < 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"MEDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+
                         break;
 
                     default:
