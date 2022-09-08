cadeaec7d53c50becc93654ccbd502af9d9e9745
Leonardo Santos <leonardojosantos@gmail.com>
2022-09-08 17:17:44 -0300

    Praticando primeiros passos

---
+++ Projeto_final/Aluno.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+namespace Projeto_final
+{
+    public struct Aluno
+    {
+
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+
+    }
+    
+}
--- Projeto_final/Program.cs
+++ Projeto_final/Program.cs
@@ -10,25 +10,45 @@ namespace Projeto_final
         static void Main(string[] args)
         {
             Console.Write("\n");
-            Console.WriteLine("Informe a opção desejada:");
-            Console.WriteLine("1 - Inserir novo aluno");
-            Console.WriteLine("2 - Listar alunos");
-            Console.WriteLine("3 - Calcular média geral");
-            Console.WriteLine("X - Sair");
-            Console.WriteLine();
 
-            string opcaoUsuario = Console.ReadLine();
+            Aluno[] alunos = new Aluno[5];
+            
+            var indiceAluno = 0;
+
+            string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
             {
                 switch (opcaoUsuario)
                 {
                     case "1":
-                        //TODO: Adicionar aluno
+                        Console.Write("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.Write("Informe a nota do aluno: ");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve serdecimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
                         break;
 
                     case "2":
-                        //TODO: Listar alunos
+                        foreach (var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
                         break;
 
                     case "3":
@@ -39,19 +59,26 @@ namespace Projeto_final
                         throw new ArgumentOutOfRangeException();
                 }
 
-                Console.WriteLine("Informe a opção desejada:");
-                Console.WriteLine("1 - Inserir novo aluno");
-                Console.WriteLine("2 - Listar alunos");
-                Console.WriteLine("3 - Calcular média geral");
-                Console.WriteLine("X - Sair");
-                Console.WriteLine();
-
-                opcaoUsuario = Console.ReadLine();
+                opcaoUsuario = ObterOpcaoUsuario();
 
             }
 
         }
 
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
     }
 
 }
